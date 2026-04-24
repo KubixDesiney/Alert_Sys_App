af33ba8294e3fb39e04e297ca1164a53325ca9d6
KubixDesiney <44655827+KubixDesiney@users.noreply.github.com>
2026-04-25 00:43:45 +0100

    Auto-Update / one push notifcation update

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ Future<void> _initOneSignalPostLaunch() async {
   if (kIsWeb) return;
   try {
     OneSignal.Debug.setLogLevel(OSLogLevel.none);
-    OneSignal.initialize("os_v2_app_givlzn6e4vddbai7ztvinjxuqh57nrdtotbej4ed7ua5hchr3y3c5ajdofhf6ykchqrsyg6c2n2igiah7c4y53bt2qls5iojftxpjua");
+    OneSignal.initialize("os_v2_app_givlzn6e4vddbai7ztvinjxuqg7cngo72pgul5emvcif2bl66awz4ld6sj76wsbrffbmbsixas6bff7q4duis6vbx3r73unkmnxvpoq");
 
     // Prevent heavy restoration backlog from blocking first frames.
     OneSignal.Notifications.clearAll();
