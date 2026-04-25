7d158a3e9923f0166fe6aa5fe0862aa0f46a298c
KubixDesiney <44655827+KubixDesiney@users.noreply.github.com>
2026-04-25 04:43:08 +0100

    auto update

---
+++ lib/services/fcm_service.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class FcmService {
+  final FirebaseMessaging _fcm = FirebaseMessaging.instance;
+  final DatabaseReference _db = FirebaseDatabase.instance.ref();
+
+  Future<void> init() async {
+    // Get token and save
+    await _updateToken();
+
+    // Listen for token refresh
+    _fcm.onTokenRefresh.listen((newToken) async {
+      await _saveTokenToDatabase(newToken);
+    });
+  }
+
+  Future<void> _updateToken() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return;
+    final token = await _fcm.getToken();
+    if (token != null) {
+      await _saveTokenToDatabase(token);
+    }
+  }
+
+  Future<void> _saveTokenToDatabase(String token) async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return;
+    await _db.child('users/${user.uid}').update({
+      'fcmToken': token,
+      'lastSeen': DateTime.now().toIso8601String(),
+    });
+    print('FCM token saved for user ${user.uid}');
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import file_saver
 import firebase_auth
 import firebase_core
 import firebase_database
+import firebase_messaging
 import flutter_local_notifications
 import share_plus
 import shared_preferences_foundation
@@ -23,6 +24,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
