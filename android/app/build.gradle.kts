94af71ab715c1b2e371051c983bf89c43f21fa58
KubixDesiney <44655827+KubixDesiney@users.noreply.github.com>
2026-05-03 15:03:01 +0100

    Fix: compileSdk 36, minSdk 24

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,51 +1,29 @@
-plugins {
-    id("com.android.application")
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.example.alertsysapp"
-    compileSdk = 36                     // required by flutter_tts / mobile_scanner / tflite_flutter
-    ndkVersion = "27.0.12077973"        // all NDK-linked plugins ask for this
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = true
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+buildscript {
+    ext.kotlin_version = '1.9.0'
+    repositories {
+        google()
+        mavenCentral()
     }
 
     dependencies {
-        coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
-    }
-
-    packaging {
-        jniLibs.useLegacyPackaging = true
-    }
-
-    defaultConfig {
-        applicationId = "com.example.alertsysapp"
-        minSdk = 24                     // needed by flutter_tts (and others)
-        targetSdk = 36
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("debug")
-        }
+        classpath 'com.android.tools.build:gradle:7.3.0'
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
     }
 }
 
-flutter {
-    source = "../.."
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.buildDir = '../build'
+subprojects {
+    project.buildDir = "${rootProject.buildDir}/${project.name}"
+    project.evaluationDependsOn(':app')
+}
+
+tasks.register("clean", Delete) {
+    delete rootProject.buildDir
 }
