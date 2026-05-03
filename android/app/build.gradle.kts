a272e655a19f900e637b86bbfc895d25be340667
KubixDesiney <44655827+KubixDesiney@users.noreply.github.com>
2026-05-03 14:31:42 +0100

    Fix: compileSdk 36, minSdk 24 for plugin compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.example.alertsysapp"
-    compileSdk = 24
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -36,7 +36,7 @@ android {
         applicationId = "com.example.alertsysapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = 24
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
