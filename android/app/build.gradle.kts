efba52e3e100a076796c2762dff98bce59bfd861
KubixDesiney <44655827+KubixDesiney@users.noreply.github.com>
2026-05-03 14:53:34 +0100

    Fix: compileSdk 36, minSdk 24

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ plugins {
 
 android {
     namespace = "com.example.alertsysapp"
-    compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    compileSdk = 36                     // required by flutter_tts / mobile_scanner / tflite_flutter
+    ndkVersion = "27.0.12077973"        // all NDK-linked plugins ask for this
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -32,11 +32,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.alertsysapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 24
+        minSdk = 24                     // needed by flutter_tts (and others)
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -44,8 +41,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
